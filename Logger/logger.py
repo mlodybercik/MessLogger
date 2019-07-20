e8fdf297d9ff7c668e95c8eff60f8429f6371db2
krzesu0 <przemekbarcicki@gmail.com>
2019-07-20 15:36:19 +0200

    Fixed up passing arguments, added support to sessionfiles, added support to ImageAttachment

--- Logger/logger.py
+++ Logger/logger.py
@@ -1,25 +1,25 @@
-from fbchat import Client, Message, ThreadType
-from getpass import getpass
+from fbchat import Client, Message, ThreadType, ImageAttachment
 from .lenArray import LenArray
 import time
 
+default = {
 # Max length of given thread history.
-LENGTH = 1000
+"LENGTH": 1000,
 
 # Translate from uid to name, DISCLAIMER: translating seems kinda buggy
-TRANSLATE_NAMES = False
+"TRANSLATE_NAMES": False,
 
 # Log to file
-LOG = True
+"LOG": True,
 
 # Log to console current messages
-LOG_TO_CONSOLE = True
+"LOG_TO_CONSOLE": True,
 
 # Dump contents of LenArray to thread files after logout
-DUMP_CONTENTS = True
+"DUMP_CONTENTS": True,
 
 # Import knwon names to knownUsers from a file
-IMPORT_KNOWN_NAMES = False
+"IMPORT_KNOWN_NAMES": False}
 
 # TODO:
 # log to file not to stdout
@@ -28,25 +28,33 @@ IMPORT_KNOWN_NAMES = False
 # add way of logging to file
 # add something to distinguish thread types
 
-def logToConsole(text):
-	if(LOG_TO_CONSOLE):
-		print(text)
-
 class Logger(Client):
 	arr = {}
 	knownUsers = {}
 
 	def __init__(self, *args, **kwargs):
 		super(Logger, self).__init__(*args, **kwargs)
-		# I hate doing that, but I hate having to look at useless space
+		# I hate doing that, but I hate having to look at useless space more
 		# good code > clean code (imo lol)
-		logToConsole("[I] Defining all useless functions...")
+		try:
+			for key in kwargs["params"]:
+				setattr(self, key, params[key])
+			del kwargs["params"]
+		except:
+			for key in default:
+				setattr(self, key, default[key])
+		self.logToConsole("[I] Defining all useless functions...")
 		for funct in allFunctions:
 			setattr(self, funct, lambda *args, **kwargs: None)
+		self.logToConsole("[I] Listening...")
+
+	def logToConsole(self, text):
+		if(self.LOG_TO_CONSOLE):
+			print(text)
 
 	def dumpContents(self, folder="history/"):
-		if(DUMP_CONTENTS):
-			logToConsole("[I] Dumping contents...")
+		if(self.DUMP_CONTENTS):
+			self.logToConsole("[I] Dumping contents...")
 			try:
 				import os
 				fname = time.strftime("%m_%d-%H_%M_%S")
@@ -60,11 +68,11 @@ class Logger(Client):
 						for line in dump:
 							file.write(line[2] + ": " + line[1] + "\r\n")
 			except Exception as e:
-				logToConsole("[E] Something went wrong when dupming. " + e)
+				self.logToConsole("[E] Something went wrong when dumping to file. " + e)
 
 
 	def translateName(self, author_id, threadType):
-		if TRANSLATE_NAMES:
+		if self.TRANSLATE_NAMES:
 			if author_id in self.knownUsers.keys():
 				return self.knownUsers[author_id]
 			else:
@@ -76,23 +84,32 @@ class Logger(Client):
 
 
 	def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
+		# exit in chat to "self" to exut
 		if message_object.text == "exit" and author_id == self.uid:
 			self.logout()
 
-		logToConsole("[M] {} in {}: {}".format(self.translateName(author_id, thread_type), thread_id, message_object.text))
-
+		# create empty LenArray for user who sent you a message
 		if(thread_id not in self.arr.keys()):
-			self.arr[thread_id] = LenArray(LENGTH)
+			self.arr[thread_id] = LenArray(self.LENGTH)
 
-		self.arr[thread_id].append([message_object.uid, message_object.text, author_id])
+		# if has text add text to previously created LenArray
+		if message_object.text:
+			self.logToConsole("[M] {} in {}: {}".format(self.translateName(author_id, thread_type), thread_id, message_object.text))
+			self.arr[thread_id].append([message_object.uid, message_object.text, author_id])
+		# else propably is an attachment, add all URLs to array
+		elif len(message_object.attachments) > 0:
+			for i in message_object.attachments:
+				if isinstance(i, ImageAttachment):
+					self.logToConsole("[M] {} in {}: {}".format(self.translateName(author_id, thread_type), thread_id, i.large_preview_url))
+					self.arr[thread_id].append([message_object.uid, i.large_preview_url, author_id])
 
 
 	def onMessageUnsent(self, mid, author_id, thread_id, thread_type, ts, msg, **kwargs):
 		mess = self.arr[thread_id].find(func=lambda a: True if a[0] == mid else False)
 		if mess == None:
-			logToConsole("[E] Couldnt resolve deleted message. {}".format(mid))
+			self.logToConsole("[E] Couldnt resolve deleted message. {}".format(mid))
 		else:
-			logToConsole("[D] {} in {} deleted message: {}".format(self.translateName(author_id, thread_type), thread_id, mess[1]))
+			self.logToConsole("[D] {} in {} deleted message: {}".format(self.translateName(author_id, thread_type), thread_id, mess[1]))
 
 
 ### END OF USED FUNCTIONS ###
--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from getpass import getpass
-from Logger.logger import Logger, logToConsole
-from fbchat import FBchatException
+from Logger.logger import Logger
+from fbchat import FBchatException, FBchatUserError
+import json
 
 if __name__ == "__main__":
+	cookies = None
+	try:
+		with open("session", "r") as file:
+			cookies = json.loads(file.read())
+	except:
+		pass
 	try:
 		try:
-			client = Logger(input("Login: "), getpass("Password: "))
+			if cookies:
+				try:
+					client = Logger("0", "0", session_cookies=cookies, max_tries=1)
+				except FBchatUserError:
+					client = Logger(input("Login: "), getpass("Password: "), max_tries=1)
+			else:
+				client = Logger(input("Login: "), getpass("Password: "), max_tries=1)
 		except FBchatException:
-			raise(Exception("Error during logging in, wrong password?"))
+			raise(Exception("Error during logging in, wrong password/bad cookies?"))
 		client.listen()
 	except:
 		try:
@@ -15,5 +28,4 @@ if __name__ == "__main__":
 		except:
 			raise(Exception("Error during logging out. Did you even log in?"))
 		else:
-			logToConsole("[I] Succesful logout.")
-		client.dumpContents()
+			client.dumpContents()
